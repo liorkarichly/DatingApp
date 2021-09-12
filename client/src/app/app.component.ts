c823b1485869a65eb3d07038a15bc0e82265314b
liorkarichly <liorkricheli1@gamil.com>
2021-09-12 19:30:47 +0300

    installl ngx-boostrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent implements OnInit{
 
   getUsers(){
 
-    this.http.get('http://localhost:5001/api/users')
+    this.http.get('https://localhost:5001/api/users')
     .subscribe(response => {
 
       this.users = response;
